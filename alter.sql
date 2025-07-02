149a2026c0509b438e3809fee9a4bfc5b5f7d65a
fady emil <fadymobiledev@gmail.com>
2025-07-02 11:41:34 +0300

    add Eaxample alter SQL

---
+++ alter.sql
@@ -0,0 +1,16 @@
+alter table TEST_Table
+add constraint street_name_un unique (street3)
+
+alter table TEST_Table
+add order_un int;
+
+alter table TEST_Table
+add constraint order_un_fk foreign key (order_un)
+references sales_schema.orders (order_id)
+
+alter table TEST_Table
+drop constraint FK_TEST_Table_store
+
+EXEC sp_rename 'Fady_DB' , 'Fady_TA'
+
+EXEC sp_rename 'Fady_Ta.street3' , 'City'
